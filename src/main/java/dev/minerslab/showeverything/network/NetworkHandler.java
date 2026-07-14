f178e9a013fa0ea366d78bca09eb8dc6341bee18
xxtg666 <xxtg666@gmail.com>
2026-07-14 17:10:27 +0000

    build: port Show Everything to Forge 1.19.2

--- src/main/java/dev/minerslab/showeverything/network/NetworkHandler.java
+++ src/main/java/dev/minerslab/showeverything/network/NetworkHandler.java
@@ -1,73 +1,38 @@
 package dev.minerslab.showeverything.network;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
 import dev.minerslab.showeverything.ShowEverythingMod;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
-import net.minecraftforge.fml.common.network.FMLNetworkEvent;
-import net.minecraftforge.fml.common.network.handshake.NetworkDispatcher;
-import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.network.NetworkDirection;
+import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.PacketDistributor;
+import net.minecraftforge.network.simple.SimpleChannel;
 
 public final class NetworkHandler {
-    public static final SimpleNetworkWrapper CHANNEL = NetworkRegistry.INSTANCE.newSimpleChannel(ShowEverythingMod.MOD_ID);
-    private static final Map<UUID, Boolean> CLIENT_MODS = new HashMap<UUID, Boolean>();
+    private static final String PROTOCOL_VERSION = "1";
+    private static final ResourceLocation CHANNEL_NAME = new ResourceLocation(ShowEverythingMod.MOD_ID, "main");
+    public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
+            CHANNEL_NAME,
+            () -> PROTOCOL_VERSION,
+            NetworkRegistry.acceptMissingOr(PROTOCOL_VERSION),
+            NetworkRegistry.acceptMissingOr(PROTOCOL_VERSION));
 
     private NetworkHandler() {
     }
 
     public static void register() {
-        CHANNEL.registerMessage(ShowItemChatMessage.Handler.class, ShowItemChatMessage.class, 0, Side.CLIENT);
+        CHANNEL.messageBuilder(ShowItemChatMessage.class, 0, NetworkDirection.PLAY_TO_CLIENT)
+                .encoder(ShowItemChatMessage::encode)
+                .decoder(ShowItemChatMessage::decode)
+                .consumerMainThread(ShowItemChatMessage::handle)
+                .add();
     }
 
-    public static boolean hasClientMod(EntityPlayerMP player) {
-        Boolean cached = CLIENT_MODS.get(player.getUniqueID());
-        if (cached != null) {
-            return cached;
-        }
-        boolean detected = detectClientMod(player);
-        CLIENT_MODS.put(player.getUniqueID(), detected);
-        return detected;
+    public static boolean hasClientMod(ServerPlayer player) {
+        return CHANNEL.isRemotePresent(player.connection.connection);
     }
 
-    private static boolean detectClientMod(EntityPlayerMP player) {
-        NetworkDispatcher dispatcher = NetworkDispatcher.get(player.connection.netManager);
-        if (dispatcher == null) {
-            return false;
-        }
-        String version = dispatcher.getModList().get(ShowEverythingMod.MOD_ID);
-        return ShowEverythingMod.VERSION.equals(version);
-    }
-
-    public static void cacheClientMod(EntityPlayerMP player) {
-        boolean hasClientMod = detectClientMod(player);
-        CLIENT_MODS.put(player.getUniqueID(), hasClientMod);
-        ShowEverythingMod.LOGGER.info("show-everything client: player={} installed={}", player.getName(), hasClientMod);
-    }
-
-    public static void clearClientMod(EntityPlayerMP player) {
-        CLIENT_MODS.remove(player.getUniqueID());
-    }
-
-    public static class Events {
-        @SubscribeEvent
-        public void onLogin(FMLNetworkEvent.ServerConnectionFromClientEvent event) {
-            if (event.getHandler() instanceof net.minecraft.network.NetHandlerPlayServer) {
-                EntityPlayerMP player = ((net.minecraft.network.NetHandlerPlayServer) event.getHandler()).player;
-                cacheClientMod(player);
-            }
-        }
-
-        @SubscribeEvent
-        public void onDisconnect(FMLNetworkEvent.ServerDisconnectionFromClientEvent event) {
-            if (event.getHandler() instanceof net.minecraft.network.NetHandlerPlayServer) {
-                EntityPlayerMP player = ((net.minecraft.network.NetHandlerPlayServer) event.getHandler()).player;
-                clearClientMod(player);
-            }
-        }
+    public static void sendTo(ShowItemChatMessage message, ServerPlayer player) {
+        CHANNEL.send(PacketDistributor.PLAYER.with(() -> player), message);
     }
 }
--- src/main/java/dev/minerslab/showeverything/network/client/ClientMessageBridge.java
+++ src/main/java/dev/minerslab/showeverything/network/client/ClientMessageBridge.java
@@ -3,31 +3,18 @@ package dev.minerslab.showeverything.network.client;
 import dev.minerslab.showeverything.network.ShowItemChatMessage;
 import dev.minerslab.showeverything.util.ChatComponents;
 import net.minecraft.client.Minecraft;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.Style;
-import net.minecraft.util.text.TextComponentString;
-import net.minecraft.util.text.event.ClickEvent;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 
 public final class ClientMessageBridge {
     private ClientMessageBridge() {
     }
 
-    public static void handleShowItemChat(final ShowItemChatMessage message) {
-        Minecraft.getMinecraft().addScheduledTask(new Runnable() {
-            @Override
-            public void run() {
-                ITextComponent line = new TextComponentString("");
-                ITextComponent senderName = new TextComponentString(message.senderName);
-                senderName.setStyle(new Style().setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/msg " + message.senderCommandName + " ")));
-                line.appendSibling(senderName);
-                line.appendText(": ");
-                line.appendSibling(ChatComponents.item(message.stack));
-                try {
-                    line.appendSibling(ITextComponent.Serializer.jsonToComponent(message.messageSuffix));
-                } catch (Exception ignored) {
-                }
-                Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(line);
-            }
-        });
+    public static void handleShowItemChat(ShowItemChatMessage message) {
+        Minecraft minecraft = Minecraft.getInstance();
+        MutableComponent line = ChatComponents.chatLine(
+                Component.literal(message.senderName), message.senderCommandName, ChatComponents.item(message.stack));
+        line.append(message.messageSuffix.copy());
+        minecraft.gui.getChat().addMessage(line);
     }
 }
